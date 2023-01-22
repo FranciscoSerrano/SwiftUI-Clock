90e3e039d14c7a19564a22bd913ad824cb731213
Francisco Serrano <ciscoserrano@icloud.com>
2023-01-22 14:51:33 -0800

    we got a circle baby

--- SwiftyClock/ContentView.swift
+++ SwiftyClock/ContentView.swift
@@ -12,6 +12,22 @@ struct ContentView: View {
         TimelineView(.animation) { timeline in
             Canvas { context, size in
                 let clock = ClockAngles(for: timeline.date)
+                
+                let drawRect = CGRect(origin: .zero, size: size)
+                let radius = min(size.width, size.height) / 2
+                
+                let boarderThickness = radius / 25
+                let innerBlackRingSize = radius / 6
+                let centerSize = radius / 40
+                
+                let hourHandLength = radius / 2.5
+                let minuteHandLength = radius / 1.5
+                
+                let secondHandLength = radius * 1.1
+                let secondHandWidth = radius / 25
+                
+                context.stroke(Circle().inset(by: boarderThickness / 2).path(in: drawRect), with: .color(.primary), lineWidth: boarderThickness)
+                context.translateBy(x: drawRect.maxX, y: drawRect.midY)
             }
         }
     }

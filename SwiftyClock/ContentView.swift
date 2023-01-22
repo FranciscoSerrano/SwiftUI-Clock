37839fd49bdb0c47b5dcc7fab2d7d02cd43d6beb
Francisco Serrano <ciscoserrano@icloud.com>
2023-01-22 14:37:49 -0800

    added clock data struct

--- SwiftyClock/ContentView.swift
+++ SwiftyClock/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TimelineView(.animation) { timeline in
+            Canvas { context, size in
+                let clock = ClockAngles(for: timeline.date)
+            }
         }
-        .padding()
     }
 }
 

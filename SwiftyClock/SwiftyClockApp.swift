3409552c5a779319d13a5d0ff8e9d3c4eb886bfa
Francisco Serrano <ciscoserrano@icloud.com>
2023-01-22 14:11:42 -0800

    Initial Commit

---
+++ SwiftyClock/SwiftyClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyClockApp.swift
+//  SwiftyClock
+//
+//  Created by Francisco Serrano on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
